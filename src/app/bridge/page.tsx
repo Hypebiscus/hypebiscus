d3b36c75ce8b54b75734fddefb98d41a6ce69c3a
Muhammad Najmuddin <muden@Muhammads-MacBook-Air.local>
2025-09-03 21:00:48 +0800

    Feat: Update bridge page and update new logo

--- src/app/bridge/page.tsx
+++ src/app/bridge/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import PageTemplate from "@/components/PageTemplate";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import Zeus from "@/components/bridge-components/Zeus";
-import Wormhole from "@/components/bridge-components/Wormhole";
+// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+// import Zeus from "@/components/bridge-components/Zeus";
+// import Wormhole from "@/components/bridge-components/Wormhole";
 
 const Bridge = () => {
   return (
     <PageTemplate>
-      <div className="w-full h-full flex flex-col items-center justify-start">
+      <div className="w-full h-[90vh] flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Comming Soon!</h1>
+      </div>
+      {/* <div className="w-full h-full flex flex-col items-center justify-start">
         <Tabs defaultValue="zeus" className="lg:w-[400px] w-full lg:px-0 px-4 mb-8">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="zeus">Zeus</TabsTrigger>
@@ -20,7 +23,7 @@ const Bridge = () => {
             <Wormhole />
           </TabsContent>
         </Tabs>
-      </div>
+      </div> */}
     </PageTemplate>
   );
 };
